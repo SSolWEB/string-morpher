e71fef9bafa8a36ec9da9d0a3d21ffa1b5923f61
Vinicius de Santana <vinicius.vts@gmail.com>
2025-03-23 17:43:49 -0300

    feat :add code linter (#2)
    * feat: add code linter
    
    * bot: linter code when pull request

--- src/Instances/StringMorpherInstance.php
+++ src/Instances/StringMorpherInstance.php
@@ -12,21 +12,31 @@ class StringMorpherInstance implements Stringable, JsonSerializable
 
     private string|null $string;
 
+    /**
+     * Creates a new instance of StringMorpherInstance.
+     * @param string|null $string The string to be morphed.
+     */
     public function __construct(string|null $string)
     {
         $this->string = $string;
     }
 
+    /** {@inheritDoc} */
     public function __toString(): string
     {
         return $this->string ?? '';
     }
 
+    /**
+     * return the actual string.
+     * @return string
+     */
     public function getString(): string
     {
         return $this->string;
     }
 
+    /** {@inheritDoc} */
     public function jsonSerialize(): mixed
     {
         return $this->string;
