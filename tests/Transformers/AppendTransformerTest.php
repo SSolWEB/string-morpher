6eaa8f07dc43b8dcb9eff7e83790bbe214728a15
Vinicius de Santana <vinicius.vts@gmail.com>
2026-01-21 22:23:49 -0300

    test: add append method for string concatenation
    Refs: #18

---
+++ tests/Transformers/AppendTransformerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SSolWEB\StringMorpher\Tests\Transformers;
+
+use PHPUnit\Framework\TestCase;
+use SSolWEB\StringMorpher\Transformers\AppendTransformer;
+use SSolWEB\StringMorpher\StringMorpher as SM;
+use SSolWEB\StringMorpher\Instances\StringMorpherInstance;
+
+class AppendTransformerTest extends TestCase
+{
+    public function testTransform()
+    {
+        $transformer = new AppendTransformer();
+
+        $tests = [
+            ['Hello World', 'Hello', ' World'],
+            ['Hello123', 'Hello', '123'],
+            ['Hello', 'Hello', ''],
+            [' World', '', ' World'],
+        ];
+
+        foreach ($tests as $test) {
+            $expected = array_shift($test);
+            $input = array_shift($test);
+            $actual = $transformer->transform($input, ...$test);
+            $this->assertEquals($expected, $actual);
+        }
+    }
+
+    public function testFacade()
+    {
+        $tests = [
+            'Hello World' => ['Hello', ' World'],
+            'Hello123' => ['Hello', '123'],
+            'Hello' => ['Hello', ''],
+            ' World' => ['', ' World'],
+            'Hello123' => ['Hello', 123],
+            'World' => [null, 'World'],
+        ];
+
+        foreach ($tests as $expected => $params) {
+            $actual = SM::append(...$params);
+            $this->assertEquals($expected, $actual);
+            $this->assertInstanceOf(StringMorpherInstance::class, $actual);
+        }
+    }
+}
