8a1dd2aa8fa8a377ced281efc61ff27f2bc756f4
Vinicius de Santana <vinicius.vts@gmail.com>
2026-04-21 19:35:08 -0300

    test: edge cases like empty, null...
    Refs: #28

--- tests/Transformers/EncodeUrlTransformerTest.php
+++ tests/Transformers/EncodeUrlTransformerTest.php
@@ -11,39 +11,45 @@ use SSolWEB\StringMorpher\Transformers\EncodeUrlTransformer;
 
 class EncodeUrlTransformerTest extends TestCase
 {
+    public static function paramProvider(): array
+    {
+        return [
+            ['Love%20%26%20PHP', 'Love & PHP'],
+            ['user%40example.com', 'user@example.com'],
+            ['Hello%20World%21%20%20%23test', 'Hello World!  #test'],
+            ['Caf%C3%A9%20M%C3%BCnster', 'Café Münster'],
+            ['abc-_.~123', 'abc-_.~123'],
+            ['', ''],
+            ['%20', ' '],
+        ];
+    }
     public function testTransform()
     {
         $transformer = new EncodeUrlTransformer();
 
-        $tests = [
-            'Love%20%26%20PHP' => 'Love & PHP',
-            'user%40example.com' => 'user@example.com',
-            'Hello%20World%21%20%20%23test' => 'Hello World!  #test',
-            'Caf%C3%A9%20M%C3%BCnster' => 'Café Münster',
-            'abc-_.~123' => 'abc-_.~123',
-        ];
-
-        foreach ($tests as $expected => $input) {
-            $actual = $transformer->transform($input);
-            $this->assertEquals($expected, $actual);
+        foreach ($this->paramProvider() as $args) {
+            $expected = array_shift($args);
+            $result = $transformer->transform(...$args);
+            $this->assertEquals($expected, $result);
         }
     }
 
     public function testFacade()
     {
-        $tests = [
-            'Love%20%26%20PHP' => ['Love & PHP'],
-            'user%40example.com' => ['user@example.com'],
-            'Hello%20World%21%20%20%23test' => ['Hello World!  #test'],
-            'Caf%C3%A9%20M%C3%BCnster' => ['Café Münster'],
-            'abc-_.~123' => ['abc-_.~123'],
-        ];
-
-        foreach ($tests as $expected => $params) {
-            $actual = SM::encodeUrl(...$params);
-            $this->assertEquals($expected, $actual);
-            $this->assertInstanceOf(StringMorpherInstance::class, $actual);
+        foreach ($this->paramProvider() as $args) {
+            $expected = array_shift($args);
+            $result = SM::encodeUrl(...$args);
+            $this->assertEquals($expected, $result);
+            $this->assertInstanceOf(StringMorpherInstance::class, $result);
         }
+    }
+    
+
+    public function testFacadeAcceptsNull()
+    {
+        $result = SM::encodeUrl(null);
+        $this->assertEquals('', $result);
+        $this->assertInstanceOf(StringMorpherInstance::class, $result);
     }
 
     public function testFluent()
